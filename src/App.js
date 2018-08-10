0712aae6cf8f599c27976b802e62a47915b0795a
schwiddi <schwiddi@me.com>
2018-08-11 00:50:40 +0200

    cleanup

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -7,8 +6,7 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">R21 Billard App</h1>
         </header>
         <p className="App-intro">
           This will be your Billard App
