9108b439fa54744702f88d63a038a6d08cca67ee
schwiddi <schwiddi@me.com>
2018-08-20 15:30:44 +0200

    made tr render with game better

--- src/components/game.jsx
+++ src/components/game.jsx
@@ -3,7 +3,7 @@ import 'bootstrap/dist/css/bootstrap.css';
 
 class Game extends Component {
   state = {
-    _id: '5b795dfcb0532cf66e87aa31',
+    _id: '1',
     playerA: 'Bäsä',
     playerB: 'Schwiddi',
     scoreplayerA: 0,
@@ -13,37 +13,49 @@ class Game extends Component {
 
   render() {
     return (
-      <React.Fragment>
-        <tr>
-          <th scope="row">{this.state.keyid}</th>
-          <td>
-            <p className="text-right">{this.state.playerA}</p>
-          </td>
-          <td>
-            <p className="text-right">
-              {this.formatScore(this.state.scoreplayerA)}
-            </p>
-          </td>
-          <td>
-            <p className="text-left">
-              {this.formatScore(this.state.scoreplayerB)}
-            </p>
-          </td>
-          <td>
-            <p className="text-left">{this.state.playerB}</p>
-          </td>
-        </tr>
-      </React.Fragment>
+      <tr>
+        <td>
+          <p>{this.state._id}</p>
+        </td>
+        <td>
+          <p className="text-right">{this.state.playerA}</p>
+        </td>
+        <td>
+          <p className="text-right">
+            {this.formatScore(this.state.scoreplayerA)}
+          </p>
+        </td>
+        <td>
+          <p className="text-left">
+            {this.formatScore(this.state.scoreplayerB)}
+          </p>
+        </td>
+        <td>
+          <p className="text-left">{this.state.playerB}</p>
+        </td>
+      </tr>
     );
   }
 
   formatScore(score) {
     if (score === 1) {
-      return <span className="badge badge-success">{score}</span>;
+      return (
+        <span style={{ fontSize: 20 }} className="badge badge-success">
+          {score}
+        </span>
+      );
     } else if (score === 0) {
-      return <span className="badge badge-secondary">{score}</span>;
+      return (
+        <span style={{ fontSize: 20 }} className="badge badge-secondary">
+          {score}
+        </span>
+      );
     } else {
-      return <span className="badge">{score}</span>;
+      return (
+        <span style={{ fontSize: 20 }} className="badge">
+          {score}
+        </span>
+      );
     }
   }
 }
