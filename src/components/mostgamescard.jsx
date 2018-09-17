ed0f4889bc1660349993d7dec33e4ea986e8f229
schwiddi <schwiddi@me.com>
2018-09-17 17:50:24 +0200

    added initial mostgamescard

---
+++ src/components/mostgamescard.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { Card, CardTitle, CardBody } from 'reactstrap';
+
+export default class MostGamesCard extends Component {
+  render() {
+    return (
+      <Card>
+        <CardBody>
+          <CardTitle>bla</CardTitle>
+        </CardBody>
+      </Card>
+    );
+  }
+}
