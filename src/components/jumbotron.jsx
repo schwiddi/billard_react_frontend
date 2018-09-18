ab4a0012a5641e038a974f7177f0e2358aa142fa
schwiddi <schwiddi@me.com>
2018-09-18 18:58:06 +0200

    spelling

--- src/components/jumbotron.jsx
+++ src/components/jumbotron.jsx
@@ -6,7 +6,7 @@ const Jumbo = props => {
     <div>
       <Jumbotron>
         <Container fluid>
-          <h1 className="display-3">r21Billard Games App</h1>
+          <h1 className="display-3">r21Billard Games</h1>
           <p className="lead">
             this is the wolrds most amazing billard games storing app that you
             have ever seen in your hole entire life... trust me.
