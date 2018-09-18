70d25345fa9f21630ccee3922f823842e0457775
schwiddi <schwiddi@me.com>
2018-09-19 00:14:35 +0200

    bla

--- src/components/jumbotron.jsx
+++ src/components/jumbotron.jsx
@@ -8,8 +8,8 @@ const Jumbo = props => {
         <Container fluid>
           <h1 className="display-3">r21Billard Games</h1>
           <p className="lead">
-            this is the wolrds most amazing billard games storing app that you
-            have ever seen in your hole entire life... trust me.
+            this is tha wolrds most amazing billard games storing app that ya've
+            ever seen in ya hole entire life... trust me.
           </p>
         </Container>
       </Jumbotron>
