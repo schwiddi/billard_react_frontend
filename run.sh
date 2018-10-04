fd49f264d529f6271ec84d64255deb4d5ee7af6a
schwiddi <schwiddi@me.com>
2018-10-04 18:18:00 +0200

    some technical stuff

--- run.sh
+++ run.sh
@@ -1,1 +1,11 @@
-sudo PORT=80 npm start
+#!/bin/bash
+echo "i'm run.sh"
+echo "testing npm"
+if [ -x "$(command -v npm)" ];
+then
+  echo 'using standard npm ...'
+  sudo PORT=80 npm start
+else
+  echo 'using /opt/node/bin/npm ...'
+  sudo PORT=80 /opt/node/bin/npm start
+fi
