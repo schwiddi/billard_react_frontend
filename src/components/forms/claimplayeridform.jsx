bb23446dd186b9f79d7327c25802ca927e680ea7
schwiddi <schwiddi@me.com>
2018-10-05 18:29:06 +0200

    removed not need imports

--- src/components/forms/claimplayeridform.jsx
+++ src/components/forms/claimplayeridform.jsx
@@ -1,6 +1,4 @@
 import React, { Component } from 'react';
-import { withRouter } from 'react-router-dom';
-import { Button } from 'reactstrap';
 
 export default class ClaimePlayerId extends Component {
   render() {
