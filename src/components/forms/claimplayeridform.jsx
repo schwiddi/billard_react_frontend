dbf93fb6abbda02b6f5f500370bd3acfcdc77bd9
schwiddi <schwiddi@me.com>
2018-10-17 18:27:41 +0200

    massive update in claim player form

--- src/components/forms/claimplayeridform.jsx
+++ src/components/forms/claimplayeridform.jsx
@@ -1,7 +1,39 @@
 import React, { Component } from 'react';
 
+import { ListGroup, ListGroupItem } from 'reactstrap';
+
 export default class ClaimePlayerId extends Component {
+  state = {};
+
+  componentDidMount = () => {};
+
   render() {
-    return <div>yay</div>;
+    const players = this.props.players;
+    return (
+      <React.Fragment>
+        <h1>Claime Player ID</h1>
+        <p>
+          Here you can claim to be a player that has already played games on the
+          famous r21.Billiard Table!
+        </p>
+        <p />
+        <p>
+          So here are all players, the grayed out have already been picked by a
+          user..
+        </p>
+        <p />
+        <ListGroup>
+          {players.map(player => {
+            if (player.user_id)
+              return (
+                <ListGroupItem key={player.id} disabled>
+                  {player.name}
+                </ListGroupItem>
+              );
+            return <ListGroupItem key={player.id}>{player.name}</ListGroupItem>;
+          })}
+        </ListGroup>
+      </React.Fragment>
+    );
   }
 }
