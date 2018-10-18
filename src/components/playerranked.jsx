75309c09180f8c1be37c2be15f34ae917022623f
Dominik Schwitter <dominik.schwitter@postfinance.ch>
2018-10-18 16:53:34 +0200

    added you label on ranking table

--- src/components/playerranked.jsx
+++ src/components/playerranked.jsx
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
+import { Badge } from 'reactstrap';
 import 'bootstrap/dist/css/bootstrap.css';
 
 class PlayerRanked extends Component {
   render() {
+    const { user } = this.props;
     let index = Number(this.props.index);
     const rank = index + 1;
     return (
@@ -10,7 +12,12 @@ class PlayerRanked extends Component {
         <th className="text-left" scope="row">
           {rank}
         </th>
-        <td className="text-left">{this.props.player.name}</td>
+        <td className="text-left">
+          {this.props.player.name}{' '}
+          {this.props.player.name === user.name && (
+            <Badge color="primary">you</Badge>
+          )}
+        </td>
         <td className="text-center">{this.props.player.games_won}</td>
         <td className="text-center">{this.props.player.games_total}</td>
         <td className="text-center">
--- src/components/playerunranked.jsx
+++ src/components/playerunranked.jsx
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
+import { Badge } from 'reactstrap';
 import 'bootstrap/dist/css/bootstrap.css';
 
 class PlayerUnranked extends Component {
   render() {
+    const { user } = this.props;
     return (
       <tr>
-        <td className="text-left">{this.props.player.name}</td>
+        <td className="text-left">
+          {this.props.player.name}{' '}
+          {this.props.player.name === user.name && (
+            <Badge color="primary">you</Badge>
+          )}
+        </td>
         <td className="text-center">{this.props.player.games_won}</td>
         <td className="text-center">{this.props.player.games_total}</td>
         <td className="text-center">
